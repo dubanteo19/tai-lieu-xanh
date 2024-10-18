04bbe1a669dfeae63c3c7104deb5d331734879fa
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2024-10-18 12:27:16 +0700

    init project

---
+++ src/main/java/com/nlu/tai_lieu_xanh/TaiLieuXanhApplication.java
@@ -0,0 +1,13 @@
+package com.nlu.tai_lieu_xanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiLieuXanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaiLieuXanhApplication.class, args);
+	}
+
+}
